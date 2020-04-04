abb175568b315ab497c670ff3ce20b26bd559e84
johnny <johnny.chanchikuen@gmail.com>
2020-04-04 21:56:38 +0800

    could function locally
    html5 video player with subtitle, progress could be stored at storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  OnDestroy,
+  HostListener
+} from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'video-web-player-angular';
+export class AppComponent implements OnDestroy {
+  // title = 'video-web-player-angular';
+
+  name = '';
+  playerSrc: SafeResourceUrl | string = '';
+  trackSrc: SafeResourceUrl | string = '';
+
+  @ViewChild('videoFileInput')
+  videoFileInput: ElementRef;
+
+  @ViewChild('subtitleFileInput')
+  subtitleFileInput: ElementRef;
+
+  @ViewChild('videoTag')
+  videoTag: ElementRef;
+
+  @ViewChild('subtitleTag')
+  subtitleTag: ElementRef;
+
+  videoFile: File | null = null;
+  subtitleFile: File | null = null;
+
+  progress = 0.0;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  @HostListener('window:beforeunload')
+  ngOnDestroy() {
+    this.saveProgressToLocalStorage();
+  }
+
+  saveProgressToLocalStorage() {
+    localStorage.setItem(this.name, this.videoTag?.nativeElement?.currentTime);
+  }
+
+  retrieveProgressFromLocalStorage() {
+    this.progress = +localStorage.getItem(this.name) || 0;
+  }
+
+  onClickVideoFileInputButton(): void {
+    if (this.videoTag) {
+      console.log(this.videoTag);
+      console.log(this.videoTag.nativeElement.currentTime);
+    }
+    this.videoFileInput.nativeElement.click();
+  }
+
+  onChangeVideoFileInput(): void {
+    const videoFiles: { [key: string]: File } = this.videoFileInput
+      .nativeElement.files;
+    this.videoFile = videoFiles[0];
+    if (!this.videoFile) {
+      return;
+    }
+    if (this.videoTag) {
+      this.saveProgressToLocalStorage();
+    }
+    this.name = this.videoFile.name;
+    this.retrieveProgressFromLocalStorage();
+    this.playerSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
+      window.URL.createObjectURL(this.videoFile) + '#t=' + this.progress
+    );
+    if (this.videoTag) {
+      this.videoTag.nativeElement.load();
+    }
+  }
+
+  onClickSubtitleFileInputButton(): void {
+    if (this.subtitleTag) {
+      console.log(this.subtitleTag);
+      console.log(this.subtitleTag.nativeElement.currentTime);
+    }
+    this.subtitleFileInput.nativeElement.click();
+  }
+
+  onChangeSubtitleFileInput(): void {
+    const subtitleFiles: { [key: string]: File } = this.subtitleFileInput
+      .nativeElement.files;
+    this.subtitleFile = subtitleFiles[0];
+    if (!this.subtitleFile) {
+      return;
+    }
+    this.name = this.subtitleFile.name;
+    this.trackSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
+      window.URL.createObjectURL(this.subtitleFile)
+    );
+    if (this.subtitleTag) {
+      this.subtitleTag.nativeElement.load();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
